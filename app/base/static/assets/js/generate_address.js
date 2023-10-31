ed7b93799fad203ab8c25e28d61314530d883ca1
dev@poof.io <105766980+poofpayments@users.noreply.github.com>
2023-10-31 00:45:22 -0700

    Cleaned up documentation

--- app/base/static/assets/js/generate_address.js
+++
@@ -1,5 +0,0 @@
-const rippleKeypairs = require('ripple-keypairs');
-
-// Assuming you derive the public key using some method
-const publicKey = '';
-console.log(rippleKeypairs.deriveAddress(publicKey));
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 """
-Copyright (c) 2019 - present AppSeed.us
+Flask Template provided by AppSeed.us
 """
 
 import os
